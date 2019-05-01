9c83597359963eaf9310bd59424c65120fcf49e3
Enrique Azorín <enrique.azorin.m@gmail.com>
2019-05-01 09:39:52 +0200

    add quasar framework as vue plugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
 import '@/firebase/';
+
+import './styles/quasar.styl';
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css';
+
+import { Quasar } from 'quasar';
+
+Vue.use(Quasar, {
+    config: {},
+    directives: {},
+    plugins: {},
+});
 
 Vue.config.productionTip = false;
 
